adf26ccdc8c182bcc9ea6a2ae13fce56c7be84e0
1095222570 <1095222570@qq.com>
2021-02-24 13:54:12 +0800

    msgqueue pro配置

--- src/msgqueue/conf.py
+++ src/msgqueue/conf.py
@@ -16,7 +16,7 @@ if env == "TEST":
 
     log_path = "/data/logs/{}/msgqueue".format(project_name)
     mysql_user = "root"
-    RedisDb = 0
+    RedisDb = 3
 
 elif env == "DEV":
     mysql_host = '127.0.0.1'
@@ -41,21 +41,12 @@ else:
     log_path = "/data/logs/{}/msgqueue".format(project_name)
     mysql_user = "root"
 
-    Productkey = 'a1agDqZJSpQ'
-    ProductHost = 'a1agDqZJSpQ.iot-as-mqtt.cn-shanghai.aliyuncs.com'
-    ProductSecret = 'SRXmMyDqCb9iIzez'
-    DeviceSecret = '4c1f6d157b5f518585bb363acbec0071'  # 'CVAp97tAUayElJwRbiXAiTgcoCZZAX1j'
-
-    OSSDomain = 'https://pangang-nontouch_1.oss-cn-beijing.aliyuncs.com'
-    OSSAccessKeyId = 'LTAI4GKiKL9WaoezWEPy8xE6'
-    OSSAccessKeySecret = 'GfuAptYtx2wIbX0vy7xAdmRoGOwo7r'
-    OSSEndpoint = 'http://oss-cn-beijing.aliyuncs.com'
-    OSSBucketName = 'pangang-nontouch_1'
-
-    MNSEndpoint = 'http://1083695407857925.mns.cn-shanghai.aliyuncs.com/'
-    MNSAccessKeyId = 'LTAI4GHswktoXpmVTnYnkEnf'
-    MNSAccessKeySecret = 'oW4jtV1FEJzcb8BnSFhhlIpSjL0E6G'
-    RedisDb = 0
+    OSSDomain = 'https://wgxing-pro.oss-cn-zhangjiakou.aliyuncs.com'
+    OSSAccessKeyId = 'LTAIWE5CGeOiozf7'
+    OSSAccessKeySecret = 'IGuoRIxwMlPQqJ9ujWyTvSq2em4RDj'
+    OSSEndpoint = 'oss-cn-zhangjiakou.aliyuncs.com'
+    OSSBucketName = 'wgxing-pro'
+    RedisDb = 3
 
 redis_conf = dict(host="127.0.0.1", port=6379, db=RedisDb, decode_responses=True)
 mysql_conf = dict(host=mysql_host, db="wuganxing_1", port=3306, user=mysql_user,
