6f1fc9ea363d8d592e10569f0031188fe6d0969f
1095222570 <1095222570@qq.com>
2021-12-09 20:47:54 +0800

    参数

--- src/database/Temperature.py
+++ src/database/Temperature.py
@@ -9,7 +9,7 @@ class Temperature(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     mobile = db.Column(db.String(16))
     temperature = db.Column(db.Float)
-    up_timestamp = db.Column(db.Float(9, 6))
+    up_timestamp = db.Column(db.String(16))
     device_id = db.Column(db.Integer)
     car_no = db.Column(db.String(32))
     gps = db.Column(db.String(32))
